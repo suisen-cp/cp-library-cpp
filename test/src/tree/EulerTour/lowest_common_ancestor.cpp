8f942915873dd5b26726f5dfe3d184af05bb9984
suisen-cp <suisen.13107f4@gmail.com>
2021-07-17 03:02:05 +0900

    test for EulerTour::lca

---
+++ test/src/tree/EulerTour/lowest_common_ancestor.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+
+#include <iostream>
+#include <vector>
+#include <library/tree/EulerTour.hpp>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n, q;
+    std::cin >> n >> q;
+    std::vector<std::vector<int>> g(n);
+    for (int i = 1; i < n; ++i) {
+        int p;
+        std::cin >> p;
+        g[i].push_back(p);
+        g[p].push_back(i);
+    }
+    suisen::EulerTour et(g);
+    for (int i = 0; i < q; ++i) {
+        int u, v;
+        std::cin >> u >> v;
+        std::cout << et.lca(u, v) << '\n';
+    }
+    return 0;
+}
