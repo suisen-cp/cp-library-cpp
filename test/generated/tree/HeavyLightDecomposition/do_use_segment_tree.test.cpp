d88886ffe2af33032ee1ceecc3762c53a7e2c653
suisen-cp <suisen.13107f4@gmail.com>
2021-07-17 00:18:05 +0900

    test (HLD)

---
+++ test/generated/tree/HeavyLightDecomposition/do_use_segment_tree.test.cpp
@@ -0,0 +1,448 @@
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+namespace atcoder {
+
+namespace internal {
+
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class S,
+          S (*op)(S, S),
+          S (*e)(),
+          class F,
+          S (*mapping)(F, S),
+          F (*composition)(F, F),
+          F (*id)()>
+struct lazy_segtree {
+  public:
+    lazy_segtree() : lazy_segtree(0) {}
+    explicit lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
+    explicit lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        lz = std::vector<F>(size, id());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return e();
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        S sml = e(), smr = e();
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
+    }
+
+    S all_prod() { return d[1]; }
+
+    void apply(int p, F f) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = mapping(f, d[p]);
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return;
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        {
+            int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
+
+    template <bool (*g)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return g(x); });
+    }
+    template <class G> int max_right(int l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (int i = log; i >= 1; i--) push(l >> i);
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*g)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return g(x); });
+    }
+    template <class G> int min_left(int r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+    std::vector<F> lz;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, F f) {
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id();
+    }
+};
+
+}  // namespace atcoder
+
+#include <iostream>
+#include <limits>
+#include <type_traits>
+#include <vector>
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2450"
+
+#ifndef SUISEN_TYPE_TRITS
+#define SUISEN_TYPE_TRITS
+
+
+namespace suisen {
+// ! utility
+template <typename ...Types>
+using constraints_t = std::enable_if_t<std::conjunction_v<Types...>, nullptr_t>;
+
+// ! function
+template <typename ReturnType, typename Callable, typename ...Args>
+using is_same_as_invoke_result = std::is_same<std::invoke_result_t<Callable, Args...>, ReturnType>;
+template <typename F, typename T>
+using is_uni_op = is_same_as_invoke_result<T, F, T>;
+template <typename F, typename T>
+using is_bin_op = is_same_as_invoke_result<T, F, T, T>;
+
+template <typename Comparator, typename T>
+using is_comparator = std::is_same<std::invoke_result_t<Comparator, T, T>, bool>;
+
+// ! integral
+template <typename T, typename = constraints_t<std::is_integral<T>>>
+constexpr int bit_num = std::numeric_limits<std::make_unsigned_t<T>>::digits;
+template <typename T, unsigned int n>
+struct is_nbit { static constexpr bool value = bit_num<T> == n; };
+template <typename T, unsigned int n>
+static constexpr bool is_nbit_v = is_nbit<T, n>::value;
+} // namespace suisen
+
+#endif
+namespace suisen {
+class HeavyLightDecomposition {
+    public:
+        template <typename Q>
+        using is_point_update_query = std::is_invocable<Q, int>;
+        template <typename Q>
+        using is_range_update_query = std::is_invocable<Q, int, int>;
+        template <typename Q, typename T>
+        using is_point_get_query  = std::is_same<std::invoke_result_t<Q, int>, T>;
+        template <typename Q, typename T>
+        using is_range_fold_query = std::is_same<std::invoke_result_t<Q, int, int>, T>;
+
+        using Graph = std::vector<std::vector<int>>;
+
+        HeavyLightDecomposition(Graph &g, int root = 0) : n(g.size()), root(root), visit(n), leave(n), head(n), ord(n), siz(n), par(n) {
+            dfs(g, root, -1);
+            int time = 0;
+            hld(g, root, -1, time);
+        }
+        int lca(int u, int v) {
+            for (;; v = par[head[v]]) {
+                if (visit[u] > visit[v]) std::swap(u, v);
+                if (head[u] == head[v]) return u;
+            }
+        }
+        int la(int u, int k, int default_value = -1) {
+            for (int h = head[u]; u >= 0; u = par[h], h = head[u]) {
+                if (visit[u] - k >= visit[h]) return ord[visit[u] - k];
+                k -= visit[u] - visit[h] + 1;
+            }
+            return default_value;
+        }
+        template <typename T, typename Q, typename F, constraints_t<is_range_fold_query<Q, T>, is_bin_op<F, T>> = nullptr>
+        T fold_path(int u, int v, T identity, F bin_op, Q fold_query, bool is_edge_query = false) {
+            T res = identity;
+            for (;; v = par[head[v]]) {
+                if (visit[u] > visit[v]) std::swap(u, v);
+                if (head[u] == head[v]) break;
+                res = bin_op(fold_query(visit[head[v]], visit[v] + 1), res);
+            }
+            return bin_op(fold_query(visit[u] + is_edge_query, visit[v] + 1), res);
+        }
+        template <
+            typename T, typename Q1, typename Q2, typename F,
+            constraints_t<is_range_fold_query<Q1, T>, is_range_fold_query<Q2, T>, is_bin_op<F, T>> = nullptr
+        >
+        T fold_path_noncommutative(int u, int v, T identity, F bin_op, Q1 fold_query, Q2 fold_query_rev, bool is_edge_query = false) {
+            T res_u = identity, res_v = identity;
+            // a := lca(u, v)
+            // res = fold(u -> a) + fold(a -> v)
+            while (head[u] != head[v]) {
+                if (visit[u] < visit[v]) { // a -> v
+                    res_v = bin_op(fold_query(visit[head[v]], visit[v] + 1), res_v);
+                    v = par[head[v]];
+                } else { // u -> a
+                    res_u = bin_op(res_u, fold_query_rev(visit[head[u]], visit[u] + 1));
+                    u = par[head[u]];
+                }
+            }
+            if (visit[u] < visit[v]) { // a = u
+                res_v = bin_op(fold_query(visit[u] + is_edge_query, visit[v] + 1), res_v);
+            } else { // a = v
+                res_u = bin_op(res_u, fold_query_rev(visit[v] + is_edge_query, visit[u] + 1));
+            }
+            return bin_op(res_u, res_v);
+        }
+        template <typename Q, constraints_t<is_range_update_query<Q>> = nullptr>
+        void update_path(int u, int v, Q update_query, bool is_edge_query = false) {
+            for (;; v = par[head[v]]) {
+                if (visit[u] > visit[v]) std::swap(u, v);
+                if (head[u] == head[v]) break;
+                update_query(visit[head[v]], visit[v] + 1);
+            }
+            update_query(visit[u] + is_edge_query, visit[v] + 1);
+        }
+        template <typename T, typename Q, constraints_t<is_range_fold_query<Q, T>> = nullptr>
+        T fold_subtree(int u, Q fold_query) {
+            return fold_query(visit[u], leave[u]);
+        }
+        template <typename Q, constraints_t<is_range_update_query<Q>> = nullptr>
+        void update_subtree(int u, Q update_query) {
+            update_query(visit[u], leave[u]);
+        }
+        template <typename T, typename Q, constraints_t<is_point_get_query<Q, T>> = nullptr>
+        T get_point(int u, Q get_query) {
+            return get_query(visit[u]);
+        }
+        template <typename Q, constraints_t<is_point_update_query<Q>> = nullptr>
+        void update_point(int u, Q update_query) {
+            update_query(visit[u]);
+        }
+    private:
+        const int n, root;
+        std::vector<int> visit, leave, head, ord, siz, par;
+        int dfs(Graph &g, int u, int p) {
+            par[u] = p;
+            siz[u] = 1;
+            int max_size = 0;
+            for (int &v : g[u]) {
+                if (v == p) continue;
+                siz[u] += dfs(g, v, u);
+                if (max_size < siz[v]) {
+                    max_size = siz[v];
+                    std::swap(g[u].front(), v);
+                }
+            }
+            return siz[u];
+        }
+        void hld(Graph &g, int u, int p, int &time) {
+            visit[u] = time, ord[time] = u, ++time;
+            head[u] = p >= 0 and g[p].front() == u ? head[p] : u;
+            for (int v : g[u]) {
+                if (v != p) hld(g, v, u, time);
+            }
+            leave[u] = time;
+        }
+};
+} // namespace suisen
+struct S {
+    int len;
+    long long pref, max, suff, sum;
+    S(int len, long long pref, long long max, long long suff, long long sum) : len(len), pref(pref), max(max), suff(suff), sum(sum) {}
+};
+
+constexpr long long INF = std::numeric_limits<int>::max();
+
+S op1(const S s1, const S s2) {
+    int len = s1.len + s2.len;
+    long long pref = std::max(s1.pref, s1.sum + s2.pref);
+    long long max  = std::max({s1.max, s2.max, s1.suff + s2.pref});
+    long long suff = std::max(s1.suff + s2.sum, s2.suff);
+    long long sum  = s1.sum + s2.sum;
+    return {len, pref, max, suff, sum};
+}
+S op2(const S s1, const S s2) {
+    return op1(s2, s1);
+}
+S e() { 
+    return {0, -INF, -INF, -INF, 0};
+}
+S mapping(long long f, S x) {
+    int len = x.len;
+    if (f == -INF or len == 0) return x;
+    long long max = f >= 0 ? f * len : f;
+    return {len, max, max, max, f * len};
+}
+long long composition(long long f, long long g) {
+    return f == -INF ? g : f;
+}
+long long id() {
+    return -INF;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n, q;
+    std::cin >> n >> q;
+    std::vector<long long> w(n);
+    for (long long &e : w) {
+        std::cin >> e;
+    }
+    std::vector<std::vector<int>> g(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        std::cin >> u >> v;
+        --u, --v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    suisen::HeavyLightDecomposition hld(g);
+    atcoder::lazy_segtree<S, op1, e, long long, mapping, composition, id> seg1(n);
+    atcoder::lazy_segtree<S, op2, e, long long, mapping, composition, id> seg2(n);
+    for (int i = 0; i < n; ++i) {
+        hld.update_point(i, [&](int j) {
+            seg1.set(j, {1, w[i], w[i], w[i], w[i]});
+            seg2.set(j, {1, w[i], w[i], w[i], w[i]});
+        });
+    }
+    for (int i = 0; i < q; ++i) {
+        int t, a, b, c;
+        std::cin >> t >> a >> b >> c;
+        --a, --b;
+        if (t == 1) {
+            hld.update_path(a, b, [&](int l, int r) {
+                seg1.apply(l, r, c);
+                seg2.apply(l, r, c);
+            });
+        } else {
+            std::cout << hld.fold_path_noncommutative(
+                a, b, e(), op1,
+                [&](int l, int r) { return seg1.prod(l, r); },
+                [&](int l, int r) { return seg2.prod(l, r); }
+            ).max << '\n';
+        }
+    }
+    return 0;
+}
