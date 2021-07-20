23242a67d56059194918ac5acac3a8de467ae582
suisen-cp <suisen.13107f4@gmail.com>
2021-07-20 20:26:53 +0900

    polynomial_taylor_shift

---
+++ test/src/math/polynomial_taylor_shift/polynomial_taylor_shift.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/polynomial_taylor_shift"
+
+#include <iostream>
+#include <vector>
+
+#include <atcoder/modint>
+#include <atcoder/convolution>
+
+#include "library/math/polynomial_taylor_shift.hpp"
+
+using mint = atcoder::modint998244353;
+
+int main() {
+    suisen::FPS<mint>::set_multiplication([](const auto &a, const auto &b) { return atcoder::convolution(a, b); });
+
+    int n, c;
+    std::cin >> n >> c;
+    suisen::FPS<mint> f(n);
+    for (int i = 0; i < n; ++i) {
+        int coef;
+        std::cin >> coef;
+        f[i] = coef;
+    }
+    auto g = suisen::translate(f, mint(c));
+    for (int i = 0; i < n; ++i) {
+        std::cout << g[i].val() << " \n"[i == n - 1];
+    }
+    return 0;
+}
