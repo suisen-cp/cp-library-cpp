ad7f55990358f9509c3c6574c55605aa1ebbae81
suisen-cp <suisen.13107f4@gmail.com>
2021-07-25 00:34:30 +0900

    run length encoder

--- library/util/run_length_encoder.hpp
+++ library/util/run_length_encoder.hpp
@@ -5,16 +5,63 @@
 
 namespace suisen {
 template <typename Container>
-class run_length_encoder {
+class RunLengthEncoder {
     public:
         using content_type   = typename Container::value_type;
-        using value_type     = std::pair<content_type, int>;
+        using value_type     = std::pair<content_type, long long>;
         using iterator       = typename std::vector<value_type>::iterator;
         using const_iterator = typename std::vector<value_type>::const_iterator;
 
-        run_length_encoder(const Container &c) noexcept {
+        RunLengthEncoder() noexcept : _length(0), comp() {}
+        RunLengthEncoder(const Container &c) noexcept {
             compress(c, comp);
+            _length = c.size();
         }
+        long long length() const noexcept {
+            return _length;
+        }
+        void swap(RunLengthEncoder<Container>  &other) { comp.swap(other.comp); }
+        void swap(RunLengthEncoder<Container> &&other) { comp.swap(other.comp); }
+        void clear() { comp.clear(); }
+        auto begin()   const noexcept { return comp.begin();   }
+        auto end()     const noexcept { return comp.end();     }
+        auto rbegin()  const noexcept { return comp.rbegin();  }
+        auto rend()    const noexcept { return comp.rend();    }
+        auto cbegin()  const noexcept { return comp.cbegin();  }
+        auto cend()    const noexcept { return comp.cend();    }
+        auto crbegin() const noexcept { return comp.crbegin(); }
+        auto crend()   const noexcept { return comp.crend();   }
+
+        const auto& front() const { return comp.front(); }
+        const auto& back()  const { return comp.back(); }
+
+        void push_back(const content_type &val, const long long num = 1) {
+            if (comp.size() and comp.back().first == val) {
+                comp.back().second += num;
+            } else {
+                comp.emplace_back(val, num);
+            }
+            _length += num;
+        }
+
+        long long pop_back(const long long num = 1) {
+            long long rem = 0;
+            while (comp.size()) {
+                long long cnt = comp.back().second;
+                if (cnt > num) {
+                    comp.back.second -= num;
+                    return rem + num;
+                }
+                num -= cnt, rem += cnt;
+                comp.pop_back();
+            }
+            _length -= rem;
+            return rem;
+        }
+
+    private:
+        long long _length;
+        std::vector<value_type> comp;
 
         static void compress(const Container &c, std::vector<value_type> &res) {
             res.clear();
@@ -25,69 +72,9 @@ class run_length_encoder {
         }
         static std::vector<value_type> compress(const Container &c) {
             std::vector<value_type> res;
-            compress(std::forward<Container>(c), res);
+            compress(c, res);
             return res;
         }
-        static std::vector<value_type> compress(const Container &&c) {
-            std::vector<value_type> res;
-            compress(std::forward<Container>(c), res);
-            return res;
-        }
-
-        const auto& cdata() const noexcept { return comp; }
-              auto   data() const noexcept { return comp; }
-
-        int max_size() const noexcept { return comp.max_size(); }
-        int capacity() const noexcept { return comp.capacity(); }
-        int size()     const noexcept { return comp.size(); }
-        void reserve(int n) { comp.reserve(n); }
-        void shrink_to_fit() { comp.shrink_to_fit(); }
-        void resize(int n) { comp.resize(); }
-        void resize(int n, const value_type  &x) { comp.resize(n, x); }
-        void resize(int n, const value_type &&x) { comp.resize(n, x); }
-        void assign(int n, const value_type  &x) { comp.assign(n, x); }
-        void assign(int n, const value_type &&x) { comp.assign(n, x); }
-        template <typename InputIterator>
-        void assign(const InputIterator first, const InputIterator last) { comp.assign(first, last); }
-        void swap(run_length_encoder<Container>  &other) { comp.swap(other.comp); }
-        void swap(run_length_encoder<Container> &&other) { comp.swap(other.comp); }
-        void clear() { comp.clear(); }
-
-        auto begin()   const noexcept { return comp.begin();   }
-        auto end()     const noexcept { return comp.end();     }
-        auto rbegin()  const noexcept { return comp.rbegin();  }
-        auto rend()    const noexcept { return comp.rend();    }
-        auto cbegin()  const noexcept { return comp.cbegin();  }
-        auto cend()    const noexcept { return comp.cend();    }
-        auto crbegin() const noexcept { return comp.crbegin(); }
-        auto crend()   const noexcept { return comp.crend();   }
-
-        const auto& operator[](const int i) const { return comp[i]; }
-              auto& operator[](const int i)       { return comp[i]; }
-        const auto& at(const int i) const { return comp.at(i); }
-              auto& at(const int i)       { return comp.at(i); }
-        const auto& front() const { return comp.front(); }
-              auto& front()       { return comp.front(); }
-        const auto& back()  const { return comp.back(); }
-              auto& back()        { return comp.back(); }
-
-        void erase(const const_iterator pos) { comp.erase(pos); }
-        void erase(const const_iterator first, const const_iterator last) { comp.erase(first, last); }
-        void pop_back() { comp.pop_back(); }
-        void insert(const const_iterator pos, value_type  &entry) { comp.insert(pos, std::forward<value_type>(entry)); }
-        void insert(const const_iterator pos, value_type &&entry) { comp.insert(pos, std::forward<value_type>(entry)); }
-        void push_back(value_type  &entry) { comp.push_back(std::forward<value_type>(entry)); }
-        void push_back(value_type &&entry) { comp.push_back(std::forward<value_type>(entry)); }
-        template <typename ...Args>
-        void emplace(const const_iterator pos, Args  &...args) { comp.emplace(pos, std::forward<Args>(args)...); }
-        template <typename ...Args>
-        void emplace(const const_iterator pos, Args &&...args) { comp.emplace(pos, std::forward<Args>(args)...); }
-        template <typename ...Args>
-        void emplace_back(Args  &...args) { comp.emplace_back(std::forward<Args>(args)...); }
-        template <typename ...Args>
-        void emplace_back(Args &&...args) { comp.emplace_back(std::forward<Args>(args)...); }
-    private:
-        std::vector<value_type> comp;
 };
 } // namespace suisen
 
