a45f9a807b24ed0d75800c8744c0f15531e2bbe6
suisen-cp <suisen.13107f4@gmail.com>
2021-08-05 19:28:12 +0900

    dirichket convolution

--- library/convolution/dirichlet_convolution.hpp
+++ library/convolution/dirichlet_convolution.hpp
@@ -19,6 +19,24 @@ static std::vector<T> dirichlet_convolution(const std::vector<T> &a, const std::
     return c;
 }
 
+template <typename T>
+static std::vector<T> dirichlet_convolution_coprime(const std::vector<T> &a, const std::vector<T> &b, const int n) {
+    std::vector<T> c(n, T(0));
+    const int m = std::min(int(a.size()), n), k = std::min(int(b.size()), n);
+    std::vector<std::vector<int>> dp(m);
+    for (int i = 1; i < m; ++i) {
+        const int jmax = std::min((n + i - 1) / i, k);
+        dp[i].reserve(jmax);
+        dp[i].push_back(i);
+        for (int j = 1; j < jmax; ++j) {
+            int g = i > j ? dp[i - j][j] : dp[i][j - i];
+            dp[i].push_back(g);
+            if (g == 1) c[i * j] += a[i] * b[j];
+        }
+    }
+    return c;
+}
+
 } // namespace suisen
 
 
