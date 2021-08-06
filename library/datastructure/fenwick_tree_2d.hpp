a1b804e27ef1ae61d744491520e3dc4a28cde2b3
suisen-cp <suisen.13107f4@gmail.com>
2021-08-06 13:44:46 +0900

    naive 2D fenwick tree

---
+++ library/datastructure/fenwick_tree_2d.hpp
@@ -0,0 +1,59 @@
+#ifndef SUISEN_FENWICK_TREE_2D
+#define SUISEN_FENWICK_TREE_2D
+
+#include <vector>
+
+namespace suisen {
+
+template <typename T>
+class FenwickTree2D {
+    public:
+        FenwickTree2D() {}
+        explicit FenwickTree2D(int n, int m) : n(n), m(m), data(n, std::vector<T>(m, T(0))) {}
+        void add(int i, int j, T v) {
+            for (int x = i + 1; x <= n; x += (x & -x)) {
+                for (int y = j + 1; y <= m; y += (y & -y)) {
+                    data[x - 1][y - 1] += v;
+                }
+            }
+        }
+        T sum(int xl, int xr, int yl, int yr) const {
+            return sum(xr, yr) - sum(xl, yr) - sum(xr, yl) + sum(xl, yl);
+        }
+        auto operator[](std::pair<int, int> index) {
+            auto [i, j] = index;
+            struct {
+                int i, j;
+                FenwickTree2D &ft;
+                operator T() const { return ft.sum(i, i + 1, j, j + 1); }
+                auto& operator++() { return *this += 1; }
+                auto& operator--() { return *this -= 1; }
+                auto& operator+=(T val) { ft.add(i, j,  val); return *this; }
+                auto& operator-=(T val) { ft.add(i, j, -val); return *this; }
+                auto& operator*=(T val) { T cur = *this; ft.add(i, cur * val - cur); return *this; }
+                auto& operator/=(T val) { T cur = *this; ft.add(i, cur / val - cur); return *this; }
+                auto& operator%=(T val) { T cur = *this; ft.add(i, cur % val - cur); return *this; }
+                auto& operator =(T val) { T cur = *this; ft.add(i,       val - cur); return *this; }
+            } obj {i, j, *this};
+            return obj;
+        }
+        T operator()(int xl, int xr, int yl, int yr) const { return sum(xl, xr, yl, yr); }
+
+    private:
+        int n, m;
+        std::vector<std::vector<T>> data;
+
+        T sum(int xr, int yr) const {
+            T s(0);
+            for (int x = xr; x; x -= x & -x) {
+                for (int y = yr; y; y -= y & -y) {
+                    s += data[x - 1][y - 1];
+                }
+            }
+            return s;
+        }
+};
+
+} // namespace suisen
+
+#endif // SUISEN_FENWICK_TREE_2D
