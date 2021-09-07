41323bb41c642d46799a9814d27c625ce060255d
suisen-cp <suisen.13107f4@gmail.com>
2021-09-08 03:45:22 +0900

    bostan mori

--- library/math/bostan_mori.hpp
+++ library/math/bostan_mori.hpp
@@ -7,14 +7,14 @@ namespace suisen {
 template <typename mint>
 mint bostan_mori(FPS<mint> P, FPS<mint> Q, unsigned long long n) {
     auto alternate = [](FPS<mint> &&a, bool odd) -> FPS<mint>&& {
-        std::size_t i = 0;
-        for (std::size_t j = odd; j < a.size(); j += 2) a[i++] = a[j];
+        int i = 0;
+        for (int j = odd; j < a.size(); j += 2) a[i++] = a[j];
         a.erase(a.begin() + i, a.end());
         return std::move(a);
     };
     for (; n; n >>= 1) {
         FPS<mint> mQ(Q);
-        for (std::size_t i = 1; i < Q.size(); i += 2) mQ[i] = -mQ[i];
+        for (int i = 1; i < Q.size(); i += 2) mQ[i] = -mQ[i];
         P = alternate(P * mQ, n & 1);
         Q = alternate(Q * mQ,     0);
     }
@@ -22,12 +22,12 @@ mint bostan_mori(FPS<mint> P, FPS<mint> Q, unsigned long long n) {
 }
 
 template <typename mint>
-mint nth_term_of_linear_recurrence(const FPS<mint> &a, const FPS<mint> &c, const unsigned long long n) {
-    const std::size_t K = c.size();
+mint nth_term_of_linearly_recurrent_sequence(const FPS<mint> &a, const FPS<mint> &c, const unsigned long long n) {
+    const int K = c.size();
     assert(K <= a.size());
     FPS<mint> Q(K + 1);
     Q[0] = 1;
-    for (std::size_t i = 0; i < K; ++i) {
+    for (int i = 0; i < K; ++i) {
         Q[i + 1] = -c[i];
     }
     FPS<mint> P = a * Q;
---
+++ test/src/math/bostan_mori/kth_term_of_linearly_recurrent_sequence.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/kth_term_of_linearly_recurrent_sequence"
+
+#include <iostream>
+#include <atcoder/modint>
+#include <atcoder/convolution>
+
+using mint = atcoder::modint998244353;
+
+#include "library/math/bostan_mori.hpp"
+using suisen::FPS;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    FPS<mint>::set_multiplication([](const auto &a, const auto &b) { return atcoder::convolution(a, b); });
+
+    std::size_t d;
+    unsigned long long k;
+    std::cin >> d >> k;
+
+    FPS<mint> a(d), c(d);
+    for (std::size_t i = 0; i < d; ++i) {
+        unsigned int v;
+        std::cin >> v;
+        a[i] = v;
+    }
+    for (std::size_t i = 0; i < d; ++i) {
+        unsigned int v;
+        std::cin >> v;
+        c[i] = v;
+    }
+    
+    std::cout << suisen::nth_term_of_linearly_recurrent_sequence(a, c, k).val() << std::endl;
+    
+    return 0;
+}
