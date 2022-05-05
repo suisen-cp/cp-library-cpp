e54072ed78a5a94b869975a6a086dbafeafdc604
suisen-cp <suisen.13107f4@gmail.com>
2022-05-05 17:37:56 +0900

    Two Edge Connected Components

--- library/graph/two_edge_connected_components.hpp
+++ library/graph/two_edge_connected_components.hpp
@@ -4,28 +4,44 @@
 #include "library/graph/low_link.hpp"
 
 namespace suisen {
-    struct TwoEdgeConnectedComponents : LowLink {
-        std::vector<int> comp_id;
-        int comp_num;
-
-        TwoEdgeConnectedComponents() : 
-            TwoEdgeConnectedComponents(0, {}) {}
-
-        TwoEdgeConnectedComponents(const int n, const std::vector<std::pair<int, int>> &edges) : 
-            TwoEdgeConnectedComponents(n, edges.size(), [&edges](int i) { return edges[i]; }) {}
-
-        template <typename EdgeGenerator>
-        TwoEdgeConnectedComponents(const int n, const int m, EdgeGenerator edge_gen) : 
-            LowLink(n, m, edge_gen), comp_id(n, -1), comp_num(0) { dfs_for_all_connected_components(); }
-        
-        int operator[](int v) const {
-            return comp_id[v];
+    struct TwoEdgeConnectedComponents : public LowLink {
+        TwoEdgeConnectedComponents()
+            : TwoEdgeConnectedComponents(0) {}
+        TwoEdgeConnectedComponents(const int n)
+            : LowLink(n), _comp_id(_n, -1), _comp_num(0) {}
+        TwoEdgeConnectedComponents(const int n, const std::vector<std::pair<int, int>> &edges)
+            : LowLink(n, edges), _comp_id(_n, -1), _comp_num(0) {
+            dfs_for_all_connected_components();
         }
 
-        Graph reduced_forest() const {
-            Graph reduced(comp_num);
-            for (int u = 0; u < n; ++u) {
-                for (const auto &[v, edge_id] : g[u]) {
+        void build() {
+            LowLink::build();
+            dfs_for_all_connected_components();
+        }
+
+        int component_num() const {
+            assert(_built);
+            return _comp_num;
+        }
+
+        int operator[](int v) const {
+            assert(_built);
+            return _comp_id[v];
+        }
+        
+        std::vector<std::vector<int>> groups() const {
+            assert(_built);
+            std::vector<std::vector<int>> res(component_num());
+            for (int i = 0; i < _n; ++i) res[_comp_id[i]].push_back(i);
+            return res;
+        }
+
+        // v -> list of (adjacent_vertex, edge_id)
+        std::vector<std::vector<std::pair<int, int>>> reduced_forest() const {
+            assert(_built);
+            std::vector<std::vector<std::pair<int, int>>> reduced(_comp_num);
+            for (int u = 0; u < _n; ++u) {
+                for (const auto &[v, edge_id] : _g[u]) {
                     const int comp_u = (*this)[u], comp_v = (*this)[v];
                     if (comp_u != comp_v) reduced[comp_u].emplace_back(comp_v, edge_id);
                 }
@@ -34,20 +50,24 @@ namespace suisen {
         }
 
     private:
+        std::vector<int> _comp_id;
+        int _comp_num;
+
         void dfs(int u, int p) {
-            if (p >= 0 and low_link[u] <= pre_order[p]) {
-                comp_id[u] = comp_id[p];
+            if (p >= 0 and _low_link[u] <= _pre_order[p]) {
+                _comp_id[u] = _comp_id[p];
             } else {
-                comp_id[u] = comp_num++;
+                _comp_id[u] = _comp_num++;
             }
-            for (const auto &e : g[u]) {
+            for (const auto &e : _g[u]) {
                 const int v = e.first;
-                if (comp_id[v] < 0) dfs(v, u);
+                if (_comp_id[v] < 0) dfs(v, u);
             }
         }
         void dfs_for_all_connected_components() {
-            for (int i = 0; i < n; ++i) {
-                if (comp_id[i] < 0) dfs(i, -1);
+            _comp_num = 0;
+            for (int i = 0; i < _n; ++i) {
+                if (_comp_id[i] < 0) dfs(i, -1);
             }
         }
     };
--- test/src/graph/two_edge_connected_components/two_edge_connected_components.test.cpp
+++ test/src/graph/two_edge_connected_components/two_edge_connected_components.test.cpp
@@ -5,27 +5,40 @@
 #include "library/graph/two_edge_connected_components.hpp"
 using suisen::TwoEdgeConnectedComponents;
 
+auto solve1(int n, std::vector<std::pair<int, int>> edges) {
+    TwoEdgeConnectedComponents tecc(n, edges);
+    return std::make_pair(tecc.component_num(), tecc.groups());
+}
+auto solve2(int n, std::vector<std::pair<int, int>> edges) {
+    TwoEdgeConnectedComponents tecc(n);
+    for (auto &[u, v] : edges) {
+        tecc.add_edge(u, v);
+    }
+    tecc.build();
+    return std::make_pair(tecc.component_num(), tecc.groups());
+}
+
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     
     int n, m;
     std::cin >> n >> m;
+
     std::vector<std::pair<int, int>> edges(m);
-    for (auto &[u, v] : edges) {
-        std::cin >> u >> v;
-    }
-    TwoEdgeConnectedComponents tecc(n, edges);
-    std::vector<std::vector<int>> c(tecc.comp_num);
-    for (int v = 0; v < n; ++v) {
-        c[tecc[v]].push_back(v);
-    }
-    std::cout << tecc.comp_num << '\n';
-    for (const auto &vs : c) {
-        std::cout << vs.size();
-        for (const int v : vs) {
-            std::cout << ' ' << v;
-        }
+    for (auto &[u, v] : edges) std::cin >> u >> v;
+
+    auto ans1 = solve1(n, edges);
+    auto ans2 = solve2(n, edges);
+
+    assert(ans1 == ans2);
+
+    const auto &[comp_num, groups] = ans1;
+
+    std::cout << comp_num << '\n';
+    for (const auto &group : groups) {
+        std::cout << group.size();
+        for (const int v : group) std::cout << ' ' << v;
         std::cout << '\n';
     }
     return 0;
