fe4bc1f23c9ade8d45e6e4bf5aa9335f16172806
suisen-cp <suisen.13107f4@gmail.com>
2022-05-05 17:37:18 +0900

    Remove Multiedge

--- library/graph/remove_multiedges.hpp
+++ library/graph/remove_multiedges.hpp
@@ -9,14 +9,9 @@ namespace suisen {
 
 void remove_multiedges(std::vector<std::vector<int>> &g) {
     std::vector<uint8_t> exists(g.size(), 0);
-    auto rem_pred = [&](int v) {
-        bool cond = not exists[v];
-        exists[v] = false;
-        return cond;
-    };
     for (auto &vs : g) {
         for (int v : vs) exists[v] = true;
-        vs.erase(std::remove_if(vs.begin(), vs.end(), rem_pred), vs.end());
+        vs.erase(std::remove_if(vs.begin(), vs.end(), [&](int v) { return not std::exchange(exists[v], false); }), vs.end());
     }
 }
 
