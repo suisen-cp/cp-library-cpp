0a6f89279beaffa1d89314d5683465aebd14bfcb
suisen-cp <suisen.13107f4@gmail.com>
2022-06-04 19:06:22 +0900

    optimize fps when the degrees are small

--- library/polynomial/fps.hpp
+++ library/polynomial/fps.hpp
@@ -5,28 +5,27 @@
 #include <cassert>
 #include <iostream>
 
+#include "library/polynomial/fps_naive.hpp"
 #include "library/math/inv_mods.hpp"
 
 namespace suisen {
+    template <typename mint>
+    using convolution_t = std::vector<mint>(*)(const std::vector<mint>&, const std::vector<mint>&);
 
-template <typename mint>
-using convolution_t = std::vector<mint> (*)(const std::vector<mint> &, const std::vector<mint> &);
-
-template <typename mint>
-class FPS : public std::vector<mint> {
-    public:
+    template <typename mint>
+    struct FPS : public std::vector<mint> {
         using std::vector<mint>::vector;
 
         FPS(const std::initializer_list<mint> l) : std::vector<mint>::vector(l) {}
-        FPS(const std::vector<mint> &v) : std::vector<mint>::vector(v) {}
-        FPS(std::vector<mint> &&v) : std::vector<mint>::vector(std::move(v)) {}
+        FPS(const std::vector<mint>& v) : std::vector<mint>::vector(v) {}
+        FPS(std::vector<mint>&& v) : std::vector<mint>::vector(std::move(v)) {}
 
         static void set_multiplication(convolution_t<mint> multiplication) {
             FPS<mint>::mult = multiplication;
         }
 
         const mint  operator[](int n) const noexcept { return n <= deg() ? unsafe_get(n) : 0; }
-              mint& operator[](int n)       noexcept { ensure_deg(n); return unsafe_get(n); }
+        mint& operator[](int n)       noexcept { ensure_deg(n); return unsafe_get(n); }
 
         int size() const noexcept { return std::vector<mint>::size(); }
         int deg()  const noexcept { return size() - 1; }
@@ -43,30 +42,29 @@ class FPS : public std::vector<mint> {
         FPS operator+() const { return FPS(*this); }
         FPS operator-() const {
             FPS f(*this);
-            for (auto &e : f) e = mint::mod() - e;
+            for (auto& e : f) e = mint::mod() - e;
             return f;
         }
         FPS& operator++() { ++(*this)[0]; return *this; }
         FPS& operator--() { --(*this)[0]; return *this; }
         FPS& operator+=(const mint x) { (*this)[0] += x; return *this; }
         FPS& operator-=(const mint x) { (*this)[0] -= x; return *this; }
-        FPS& operator+=(const FPS &g) {
+        FPS& operator+=(const FPS& g) {
             ensure_deg(g.deg());
             for (int i = 0; i <= g.deg(); ++i) unsafe_get(i) += g.unsafe_get(i);
             return *this;
         }
-        FPS& operator-=(const FPS &g) {
+        FPS& operator-=(const FPS& g) {
             ensure_deg(g.deg());
             for (int i = 0; i <= g.deg(); ++i) unsafe_get(i) -= g.unsafe_get(i);
             return *this;
         }
-        FPS& operator*=(const FPS  &g) { return *this = FPS<mint>::mult(*this, g); }
-        FPS& operator*=(      FPS &&g) { return *this = FPS<mint>::mult(*this, g); }
+        FPS& operator*=(const FPS& g) { return *this = FPS<mint>::mult(*this, g); }
         FPS& operator*=(const mint x) {
-            for (auto &e : *this) e *= x;
+            for (auto& e : *this) e *= x;
             return *this;
         }
-        FPS& operator/=(FPS &&g) {
+        FPS& operator/=(FPS g) {
             const int fd = normalize(), gd = g.normalize();
             assert(gd >= 0);
             if (fd < gd) { this->clear(); return *this; }
@@ -82,7 +80,7 @@ class FPS : public std::vector<mint> {
             std::reverse(this->begin(), this->end());
             return *this;
         }
-        FPS& operator%=(FPS &&g) {
+        FPS& operator%=(FPS g) {
             int fd = normalize(), gd = g.normalize();
             assert(gd >= 0);
             if (fd < gd) return *this;
@@ -92,8 +90,6 @@ class FPS : public std::vector<mint> {
             *this -= g * (*this / g);
             return pre_inplace(gd - 1);
         }
-        FPS& operator/=(const FPS &g) { return *this /= FPS(g); }
-        FPS& operator%=(const FPS &g) { return *this %= FPS(g); }
         FPS& operator<<=(const int shamt) {
             this->insert(this->begin(), shamt, 0);
             return *this;
@@ -104,19 +100,19 @@ class FPS : public std::vector<mint> {
             return *this;
         }
 
-        friend FPS operator+(FPS f, const FPS &g) { f += g; return f; }
+        friend FPS operator+(FPS f, const FPS& g) { f += g; return f; }
         friend FPS operator+(FPS f, const mint x) { f += x; return f; }
-        friend FPS operator-(FPS f, const FPS &g) { f -= g; return f; }
+        friend FPS operator-(FPS f, const FPS& g) { f -= g; return f; }
         friend FPS operator-(FPS f, const mint x) { f -= x; return f; }
-        friend FPS operator*(FPS f, const FPS &g) { f *= g; return f; }
+        friend FPS operator*(FPS f, const FPS& g) { f *= g; return f; }
         friend FPS operator*(FPS f, const mint x) { f *= x; return f; }
-        friend FPS operator/(FPS f, const FPS &g) { f /= g; return f; }
-        friend FPS operator%(FPS f, const FPS &g) { f %= g; return f; }
+        friend FPS operator/(FPS f, const FPS& g) { f /= g; return f; }
+        friend FPS operator%(FPS f, const FPS& g) { f %= g; return f; }
         friend FPS operator*(const mint x, FPS f) { f *= x; return f; }
         friend FPS operator<<(FPS f, const int shamt) { f <<= shamt; return f; }
         friend FPS operator>>(FPS f, const int shamt) { f >>= shamt; return f; }
 
-        friend bool operator==(const FPS &f, const FPS &g) {
+        friend bool operator==(const FPS& f, const FPS& g) {
             int n = f.size(), m = g.size();
             if (n < m) return g == f;
             for (int i = 0; i < m; ++i) if (f.unsafe_get(i) != g.unsafe_get(i)) return false;
@@ -138,7 +134,8 @@ class FPS : public std::vector<mint> {
             return *this;
         }
         FPS& inv_inplace(const int max_deg) {
-            FPS res { unsafe_get(0).inv() };
+            if (max_deg <= 60) return *this = FPSNaive<mint>(this->begin(), this->end()).inv(max_deg);
+            FPS res{ unsafe_get(0).inv() };
             for (int k = 1; k <= max_deg; k *= 2) {
                 FPS tmp(this->pre(k * 2) * (res * res));
                 res *= 2, res -= tmp.pre_inplace(2 * k);
@@ -146,17 +143,19 @@ class FPS : public std::vector<mint> {
             return *this = std::move(res), pre_inplace(max_deg);
         }
         FPS& log_inplace(const int max_deg) {
+            if (max_deg <= 60) return *this = FPSNaive<mint>(this->begin(), this->end()).log(max_deg);
             FPS f_inv = inv(max_deg);
             diff_inplace(), *this *= f_inv, pre_inplace(max_deg - 1), intg_inplace();
             return *this;
         }
         FPS& exp_inplace(const int max_deg) {
-            if (max_deg <= 60) return *this = naive_exp(max_deg);
-            FPS res {1};
+            if (max_deg <= 60) return *this = FPSNaive<mint>(this->begin(), this->end()).exp(max_deg);
+            FPS res{ 1 };
             for (int k = 1; k <= max_deg; k *= 2) res *= ++(pre(k * 2) - res.log(k * 2)), res.pre_inplace(k * 2);
             return *this = std::move(res), pre_inplace(max_deg);
         }
         FPS& pow_inplace(const long long k, const int max_deg) {
+            if (max_deg <= 60) return *this = FPSNaive<mint>(this->begin(), this->end()).pow(k, max_deg);
             int tlz = 0;
             while (tlz <= deg() and unsafe_get(tlz) == 0) ++tlz;
             if (tlz * k > max_deg) { this->clear(); return *this; }
@@ -165,12 +164,12 @@ class FPS : public std::vector<mint> {
             *this *= base.inv(), log_inplace(max_deg), *this *= k, exp_inplace(max_deg), *this *= base.pow(k);
             return *this <<= tlz * k, pre_inplace(max_deg);
         }
-        FPS diff() const { FPS f{*this}; f.diff_inplace(); return f; }
-        FPS intg() const { FPS f{*this}; f.intg_inplace(); return f; }
-        FPS inv(const int max_deg) const { FPS f{*this}; f.inv_inplace(max_deg); return f; }
-        FPS log(const int max_deg) const { FPS f{*this}; f.log_inplace(max_deg); return f; }
-        FPS exp(const int max_deg) const { FPS f{*this}; f.exp_inplace(max_deg); return f; }
-        FPS pow(const long long k, const int max_deg) const { FPS f{*this}; f.pow_inplace(k, max_deg); return f; }
+        FPS diff() const { FPS f{ *this }; f.diff_inplace(); return f; }
+        FPS intg() const { FPS f{ *this }; f.intg_inplace(); return f; }
+        FPS inv(const int max_deg) const { FPS f{ *this }; f.inv_inplace(max_deg); return f; }
+        FPS log(const int max_deg) const { FPS f{ *this }; f.log_inplace(max_deg); return f; }
+        FPS exp(const int max_deg) const { FPS f{ *this }; f.exp_inplace(max_deg); return f; }
+        FPS pow(const long long k, const int max_deg) const { FPS f{ *this }; f.pow_inplace(k, max_deg); return f; }
 
         mint eval(mint x) const {
             mint y = 0;
@@ -183,9 +182,9 @@ class FPS : public std::vector<mint> {
         static convolution_t<mint> mult;
         void ensure_deg(int d) { if (deg() < d) this->resize(d + 1, 0); }
         const mint& unsafe_get(int i) const { return std::vector<mint>::operator[](i); }
-              mint& unsafe_get(int i)       { return std::vector<mint>::operator[](i); }
+        mint& unsafe_get(int i) { return std::vector<mint>::operator[](i); }
 
-        std::pair<FPS, FPS&> naive_div_inplace(FPS &&g, const int gd) {
+        std::pair<FPS, FPS&> naive_div_inplace(FPS&& g, const int gd) {
             const int k = deg() - gd;
             mint head_inv = g.unsafe_get(gd).inv();
             FPS q(k + 1);
@@ -194,21 +193,21 @@ class FPS : public std::vector<mint> {
                 q.unsafe_get(i) = div;
                 for (int j = 0; j <= gd; ++j) this->unsafe_get(i + j) -= div * g.unsafe_get(j);
             }
-            return {q, pre_inplace(gd - 1)};
+            return { q, pre_inplace(gd - 1) };
         }
-};
+    };
 
-template <typename mint>
-convolution_t<mint> FPS<mint>::mult = [](const auto &, const auto &) {
-    std::cerr << "convolution function is not available." << std::endl;
-    assert(false);
-    return std::vector<mint>{};
-};
+    template <typename mint>
+    convolution_t<mint> FPS<mint>::mult = [](const auto&, const auto&) {
+        std::cerr << "convolution function is not available." << std::endl;
+        assert(false);
+        return std::vector<mint>{};
+    };
 
 } // namespace suisen
 
 template <typename mint>
-auto sqrt(suisen::FPS<mint> a) -> decltype(mint::mod(), suisen::FPS<mint>{})  {
+auto sqrt(suisen::FPS<mint> a) -> decltype(mint::mod(), suisen::FPS<mint>{}) {
     assert(false);
 }
 template <typename mint>
@@ -224,7 +223,7 @@ auto pow(suisen::FPS<mint> a, T b) -> decltype(mint::mod(), suisen::FPS<mint>{})
     return a.pow(b, a.deg());
 }
 template <typename mint>
-auto inv(suisen::FPS<mint> a) -> decltype(mint::mod(), suisen::FPS<mint>{})  {
+auto inv(suisen::FPS<mint> a) -> decltype(mint::mod(), suisen::FPS<mint>{}) {
     return a.inv(a.deg());
 }
 
