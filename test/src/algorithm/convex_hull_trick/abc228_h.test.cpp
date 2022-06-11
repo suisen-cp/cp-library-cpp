22f7d3210e7d1022053b73c4122e55b36904f76b
suisen-cp <suisen.13107f4@gmail.com>
2022-06-11 19:15:48 +0900

    test file of CHT

---
+++ test/src/algorithm/convex_hull_trick/abc228_h.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://atcoder.jp/contests/abc228/tasks/abc228_h"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "library/algorithm/convex_hull_trick.hpp"
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n, x;
+    std::cin >> n >> x;
+
+    std::vector<std::pair<long long, long long>> ps(n);
+    for (auto &[a, c] : ps) std::cin >> a >> c;
+    std::sort(ps.begin(), ps.end());
+
+    suisen::ConvexHullTrick<long long> cht;
+    
+    long long ans = 0;
+    long long s = 0, t = 0;
+    cht.add_line(-s, t);
+    for (const auto &[a, c] : ps) {
+        s += c, t += a * c;
+        ans = cht.query(a) + x + a * s - t;
+        cht.add_line(-s, ans + t);
+    }
+
+    std::cout << ans << std::endl;
+    return 0;
+}
---
+++ test/src/algorithm/monotonic_convex_hull_trick/abc228_h.test.cpp
@@ -0,0 +1,38 @@
+#define PROBLEM "https://atcoder.jp/contests/abc228/tasks/abc228_h"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "library/algorithm/monotonic_convex_hull_trick.hpp"
+
+constexpr int M = 1000000;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n, x;
+    std::cin >> n >> x;
+
+    std::array<long long, M + 1> cs{};
+    for (int i = 0; i < n; ++i) {
+        int a, c;
+        std::cin >> a >> c;
+        cs[a] += c;
+    }
+
+    suisen::MonotonicCHT<long long> cht;
+    
+    long long ans = 0;
+    long long s = 0, t = 0;
+    cht.add_line(-s, t);
+    for (long long a = 0; a <= M; ++a) if (long long c = cs[a]; c) {
+        s += c, t += a * c;
+        ans = cht.ascending_query(a) + x + a * s - t;
+        cht.add_line(-s, ans + t);
+    }
+    std::cout << ans << std::endl;
+    
+    return 0;
+}
