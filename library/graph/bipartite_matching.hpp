fffb575a1c035abafa581f8975e23b4e6e4db17d
suisen-cp <suisen.13107f4@gmail.com>
2022-06-28 04:44:21 +0900

    improve constant factor of bipartite matching

--- library/graph/bipartite_matching.hpp
+++ library/graph/bipartite_matching.hpp
@@ -2,6 +2,7 @@
 #define SUISEN_BIPARTITE_MATCHING
 
 #include <algorithm>
+#include <cassert>
 #include <deque>
 #include <random>
 #include <utility>
@@ -18,32 +19,86 @@ namespace suisen {
             _g[fr].push_back(to), _f = -1;
         }
 
-        template <bool shuffle = true>
+        // template <bool shuffle = true>
+        // int solve_heuristics() {
+        //     if (_f >= 0) return _f;
+
+        //     static std::mt19937 rng(std::random_device{}());
+        //     if constexpr (shuffle) for (auto& adj : _g) std::shuffle(adj.begin(), adj.end(), rng);
+
+        //     std::vector<int8_t> vis(_n, false);
+
+        //     auto dfs = [&, this](auto dfs, int u) -> bool {
+        //         if (std::exchange(vis[u], true)) return false;
+        //         for (int v : _g[u]) if (_to_l[v] == ABSENT) return _to_r[u] = v, _to_l[v] = u, true;
+        //         for (int v : _g[u]) if (dfs(dfs, _to_l[v])) return _to_r[u] = v, _to_l[v] = u, true;
+        //         return false;
+        //     };
+
+        //     for (bool upd = true; std::exchange(upd, false);) {
+        //         vis.assign(_n, false);
+        //         for (int i = 0; i < _n; ++i) if (_to_r[i] == ABSENT) upd |= dfs(dfs, i);
+        //     }
+
+        //     return _f = _n - std::count(_to_r.begin(), _to_r.end(), ABSENT);
+        // }
+    
         int solve() {
             if (_f >= 0) return _f;
+            const auto h = reversed_graph();
 
-            static std::mt19937 rng(std::random_device{}());
-            if constexpr (shuffle) for (auto &adj : _g) std::shuffle(adj.begin(), adj.end(), rng);
-
-            std::vector<int8_t> vis(_n, false);
-        
-            auto dfs = [&, this](auto dfs, int u) -> bool {
-                if (std::exchange(vis[u], true)) return false;
-                for (int v : _g[u]) if (_to_l[v] == ABSENT) return _to_r[u] = v, _to_l[v] = u, true;
-                for (int v : _g[u]) if (dfs(dfs, _to_l[v])) return _to_r[u] = v, _to_l[v] = u, true;
-                return false;
+            std::vector<int> level(_n + _m), iter(_n + _m);
+            std::deque<int> que;
+            auto bfs = [&] {
+                for (int i = 0; i < _n; ++i) {
+                    if (_to_r[i] == ABSENT) level[i] = 0, que.push_back(i);
+                    else level[i] = -1;
+                }
+                std::fill(level.begin() + _n, level.end(), -1);
+                bool ok = false;
+                while (not que.empty()) {
+                    const int l = que.front();
+                    que.pop_front();
+                    for (int r : _g[l]) if (_to_r[l] != r and level[_n + r] < 0) {
+                        const int nl = _to_l[r];
+                        level[_n + r] = level[l] + 1;
+                        if (nl == ABSENT) ok = true;
+                        else if (level[nl] < 0) level[nl] = level[l] + 2, que.push_back(nl);
+                    }
+                }
+                return ok;
             };
-    
-            for (bool upd = true; std::exchange(upd, false);) {
-                vis.assign(_n, false);
-                for (int i = 0; i < _n; ++i) if (_to_r[i] == ABSENT) upd |= dfs(dfs, i);
+            auto dfs = [&](auto dfs, const int r) -> bool {
+                const int level_v = level[_n + r];
+                if (level_v < 0) return false;
+                const int dr = h[r].size();
+                for (int &i = iter[_n + r]; i < dr; ++i) {
+                    const int l = h[r][i];
+                    if (level_v <= level[l] or _to_l[r] == l or iter[l] > _m) continue;
+                    if (int nr = _to_r[l]; nr == ABSENT) {
+                        iter[l] = _m + 1, level[l] = _n + _m;
+                        _to_r[l] = r, _to_l[r] = l;
+                        return true;
+                    } else if (iter[l] <= nr) {
+                        iter[l] = nr + 1;
+                        if (level[l] > level[_n + nr] and dfs(dfs, nr)) {
+                            _to_r[l] = r, _to_l[r] = l;
+                            return true;
+                        }
+                        iter[l] = _m + 1, level[l] = _n + _m;
+                    }
+                }
+                return level[_n + r] = _n + _m, false;
+            };
+            for (_f = 0; bfs();) {
+                std::fill(iter.begin(), iter.end(), 0);
+                for (int j = 0; j < _m; ++j) if (_to_l[j] == ABSENT) _f += dfs(dfs, j);
             }
-
-            return _f = _n - std::count(_to_r.begin(), _to_r.end(), ABSENT);
+            return _f;
         }
 
         std::vector<std::pair<int, int>> max_matching() {
-            if (_f < 0) _f = solve();
+            if (_f < 0) solve();
             std::vector<std::pair<int, int>> res;
             res.reserve(_f);
             for (int i = 0; i < _n; ++i) if (_to_r[i] != ABSENT) res.emplace_back(i, _to_r[i]);
@@ -53,7 +108,7 @@ namespace suisen {
         std::vector<std::pair<int, int>> min_edge_cover() {
             auto res = max_matching();
             std::vector<bool> vl(_n, false), vr(_n, false);
-            for (const auto &[u, v] : res) vl[u] = vr[v] = true;
+            for (const auto& [u, v] : res) vl[u] = vr[v] = true;
             for (int u = 0; u < _n; ++u) for (int v : _g[u]) if (not (vl[u] and vr[v])) {
                 vl[u] = vr[v] = true;
                 res.emplace_back(u, v);
@@ -62,7 +117,7 @@ namespace suisen {
         }
 
         std::vector<int> min_vertex_cover() {
-            if (_f < 0) _f = solve();
+            if (_f < 0) solve();
             std::vector<std::vector<int>> g(_n + _m);
             std::vector<bool> cl(_n, true), cr(_m, false);
             for (int u = 0; u < _n; ++u) for (int v : _g[u]) {
@@ -94,7 +149,7 @@ namespace suisen {
             for (int i = 0; i < _m; ++i) if (cr[i]) res.push_back(_n + i);
             return res;
         }
-        
+
         std::vector<int> max_independent_set() {
             std::vector<bool> use(_n + _m, true);
             for (int v : min_vertex_cover()) use[v] = false;
@@ -107,12 +162,12 @@ namespace suisen {
         int right_size() const { return _m; }
         std::pair<int, int> size() const { return { _n, _m }; }
 
-        int right(int l) const { return _to_r[l]; }
-        int left(int r) const { return _to_l[r]; }
+        int right(int l) const { assert(_f >= 0); return _to_r[l]; }
+        int left(int r) const { assert(_f >= 0); return _to_l[r]; }
 
         const auto graph() const { return _g; }
 
-        auto reversed_graph() const {
+        std::vector<std::vector<int>> reversed_graph() const {
             std::vector<std::vector<int>> h(_m);
             for (int i = 0; i < _n; ++i) for (int j : _g[i]) h[j].push_back(i);
             return h;
@@ -124,7 +179,7 @@ namespace suisen {
         std::vector<std::vector<int>> _g;
         int _f = 0;
     };
-    
+
 } // namespace suisen
 
 
