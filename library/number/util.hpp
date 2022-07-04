a3f1c9170c65e30d45c8ed2cfa4f824cd5d954ab
suisen-cp <suisen.13107f4@gmail.com>
2022-07-05 04:32:23 +0900

    kth root (integer)

--- library/number/util.hpp
+++ library/number/util.hpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <cmath>
 #include <optional>
 #include <tuple>
 #include <vector>
@@ -224,6 +225,16 @@ namespace suisen {
         return res;
     }
 
+    template <typename T, std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
+    T floor_kth_root(T x, int k) {
+        if (k == 1 or x == 0 or x == 1) return x;
+        if (k >= 64) return 1;
+        if (k == 2) return sqrtl(x);
+        // if (k == 3) return cbrtl(x);
+        T res = powl(x, nextafterl(1 / (long double) k, 0));
+        while (powl(res + 1, k) <= x) ++res;
+        return res;
+    }
 } // namespace suisen
 
 #endif // SUISEN_NUMBER_UTIL
