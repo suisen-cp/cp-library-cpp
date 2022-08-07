e998ad53ee495dba9373a176953ac68961ecc99e
suisen-cp <suisen.13107f4@gmail.com>
2022-08-07 21:13:28 +0900

    fix bug of sparse segment tree

--- library/datastructure/segment_tree/sparse_segment_tree.hpp
+++ library/datastructure/segment_tree/sparse_segment_tree.hpp
@@ -5,12 +5,17 @@
 #include <vector>
 
 namespace suisen {
+    namespace internal::sparse_segtree {
+        template <typename IndexType, typename T, T(*e)()>
+        T default_init(IndexType, IndexType) { return e(); }
+    }
     template <
-        typename IndexType,            // type of index (integral: bool, long long, etc.)
-        typename T,                    // type of element
-        T(*op)(T, T),                  // type of binary operator on T
-        T(*e)(),                       //
-        T(*init)(IndexType, IndexType) // type of function which initializes product of segment [l, r)
+        typename IndexType, // type of index
+        typename T,         // type of element
+        T(*op)(T, T),       // (x, y) -> x * y
+        T(*e)(),            // () -> identity
+        // (l, r) -> init[l] * init[l+1] * ... * init[r-1]
+        T(*init)(IndexType, IndexType) = internal::sparse_segtree::default_init<IndexType, T, e>
     >
     struct SparseSegmentTree {
         using index_type = IndexType;
@@ -42,25 +47,26 @@ namespace suisen {
             assert(0 <= i and i < n);
             pool_index_type cur = root;
             for (index_type l = 0, r = n; cur and r - l > 1;) {
-                index_type m = (lr[0] + lr[1]) >> 1;
+                index_type m = (l + r) >> 1;
                 if (i < m) cur = pool[cur].ch[0], r = m;
                 else       cur = pool[cur].ch[1], l = m;
             }
-            return cur ? pool[cur].dat : init(i, i + 1);
+            return get(cur, i, i + 1);
         }
         template <typename Fun>
         void apply_fun(index_type i, Fun &&fun) {
             assert(0 <= i and i < n);
-            static std::vector<pool_index_type> path;
-            pool_index_type cur = root;
-            for (index_type l = 0, r = n; r - l > 1;) {
-                path.push_back(cur);
-                index_type m = (lr[0] + lr[1]) >> 1;
-                if (i < m) cur = get_or_create_child(cur, 0, l, m), r = m;
-                else       cur = get_or_create_child(cur, 1, m, r), l = m;
-            }
-            pool[cur].dat = fun(pool[cur].dat);
-            while (path.size()) update(path.back()), path.pop_back();
+            auto rec = [&](auto rec, pool_index_type cur, index_type l, index_type r) -> void {
+                if (r - l == 1) {
+                    pool[cur].dat = fun(get(cur, l, r));
+                    return;
+                }
+                const index_type m = (l + r) >> 1;
+                if (i < m) rec(rec, get_or_create_child(cur, 0, l, m), l, m);
+                else       rec(rec, get_or_create_child(cur, 1, m, r), m, r);
+                pool[cur].dat = op(get(pool[cur].ch[0], l, m), get(pool[cur].ch[1], m, r));
+            };
+            rec(rec, root, 0, n);
         }
         void set(index_type i, const value_type& val) {
             apply_fun(i, [&val](const value_type&) { return val; });
@@ -81,19 +87,19 @@ namespace suisen {
         index_type n;
         pool_index_type root;
 
+        value_type get(pool_index_type node, index_type tl, index_type tr) const {
+            return node ? pool[node].dat : init(tl, tr);
+        }
+
         pool_index_type get_or_create_child(pool_index_type node, int index, index_type tl, index_type tr) {
             if (pool[node].ch[index]) return pool[node].ch[index];
             const pool_index_type ch = new_node(init(tl, tr));
             return pool[node].ch[index] = ch;
         }
 
-        void update(pool_index_type node) {
-            pool_index_type lch = pool[node].ch[0], rch = pool[node].ch[1];
-            pool[node].dat = op(pool[lch].dat, pool[rch].dat);
-        }
-
         value_type query(pool_index_type node, index_type ql, index_type qr, index_type tl, index_type tr) const {
             if (tr <= ql or qr <= tl) return e();
+            if (not node) return init(std::max(ql, tl), std::min(qr, tr));
             if (ql <= tl and tr <= qr) return pool[node].dat;
             const index_type tm = (tl + tr) >> 1;
             return op(query(pool[node].ch[0], ql, qr, tl, tm), query(pool[node].ch[1], ql, qr, tm, tr));
