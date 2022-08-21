b3b99b0e178c7cd197aade976cc50254248e9d40
suisen-cp <suisen.13107f4@gmail.com>
2022-08-21 18:21:54 +0900

    improved complexity of building Permutation Tree

--- library/datastructure/permutation_tree.hpp
+++ library/datastructure/permutation_tree.hpp
@@ -1,41 +1,31 @@
 #ifndef SUISEN_PERMUTATION_TREE
 #define SUISEN_PERMUTATION_TREE
 
-#include <atcoder/lazysegtree>
-#include <deque>
-#include <iostream>
+#include <algorithm>
+#include <cassert>
 #include <limits>
+#include <utility>
+#include <vector>
 
 namespace suisen {
-
-    namespace internal::permutation_tree {
-        using S = std::pair<int, int>;
-        using F = int;
-        S op(S x, S y) { return (x.first == y.first ? x.second > y.second : x.first < y.first) ? x : y; }
-        S e() { return { std::numeric_limits<int>::max(), -1 }; }
-        S mapping(F f, S x) { return { f + x.first, x.second }; }
-        F composition(F f, F g) { return f + g; }
-        F id() { return 0; }
-        using RangeAddRangeMin = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
-    }
-
     struct PermutationTree : std::vector<PermutationTree> {
         using base_type = std::vector<PermutationTree>;
 
-        template <typename RandomAccessibleContainer>
-        PermutationTree(const RandomAccessibleContainer& p) : PermutationTree(build(p)) {}
+        PermutationTree() : PermutationTree(0, 0, 0) {}
 
-        int size() const { return vr - vl; }
+        PermutationTree(const std::vector<int>& p) : PermutationTree(build(p)) {}
 
-        int begin_index() const { return idx; }
-        int end_index() const { return idx + size(); }
+        int length() const { return len; }
+
+        int begin_index() const { return il; }
+        int end_index() const { return il + len; }
         int min_index() const { return begin_index(); }
         int max_index() const { return end_index() - 1; }
         std::pair<int, int> get_index_range() const { return { begin_index(), end_index() }; }
         std::pair<int, int> get_index_range_closed() const { return { min_index(), max_index() }; }
-    
+
         int begin_value() const { return vl; }
-        int end_value() const { return vr; }
+        int end_value() const { return vl + len; }
         int min_value() const { return begin_value(); }
         int max_value() const { return end_value() - 1; }
         std::pair<int, int> get_value_range() const { return { begin_value(), end_value() }; }
@@ -45,13 +35,18 @@ namespace suisen {
 
         friend std::ostream& operator<<(std::ostream& out, const PermutationTree& t) {
             std::vector<std::string> lines;
-            auto dfs = [&](auto dfs, const PermutationTree &u, std::size_t d) -> void {
-                if (d >= lines.size()) lines.emplace_back(t.size(), ' ');
-                if (u.size() == 1) {
+            auto dfs = [&](auto dfs, const PermutationTree& u, std::size_t d) -> void {
+                if (d >= lines.size()) lines.emplace_back(t.length(), ' ');
+                if (u.length() == 1) {
                     lines[d][u.min_index()] = '|';
                 } else {
-                    lines[d][u.min_index()] = '[';
-                    lines[d][u.max_index()] = ']';
+                    if (u.is_join_node()) {
+                        lines[d][u.min_index()] = '[';
+                        lines[d][u.max_index()] = ']';
+                    } else {
+                        lines[d][u.min_index()] = '{';
+                        lines[d][u.max_index()] = '}';
+                    }
                 }
                 for (const auto& ch : u) dfs(dfs, ch, d + 1);
             };
@@ -61,88 +56,145 @@ namespace suisen {
         }
 
     private:
-        int idx, vl, vr;
+        int len;
+        int il, vl;
         bool _is_join_node;
 
-        PermutationTree() : PermutationTree(0, 0, 0, false) {}
-        template <typename ...Args>
-        PermutationTree(int idx, int vl, int vr, bool is_join_node, Args &&...args) :
-            base_type(std::forward<Args>(args)...), idx(idx), vl(vl), vr(vr), _is_join_node(is_join_node) {}
+        PermutationTree(int len, int il, int vl) : base_type(), len(len), il(il), vl(vl), _is_join_node(true) {}
 
-        bool try_add_child(const PermutationTree& node) {
-            if (not is_join_node() or (node.vl != vr and node.vr != vl) or empty()) return false;
-            int l0 = end()[-2].vl, l1 = end()[-1].vl, l2 = node.vl;
-            if ((l0 < l1) ^ (l1 < l2)) return false;
-            push_back(node);
-            vl = std::min(vl, node.vl), vr = std::max(vr, node.vr);
-            return true;
-        }
-
-        template <typename RandomAccessibleContainer>
-        static PermutationTree build(const RandomAccessibleContainer& p) {
-            std::vector<PermutationTree> st;
+        static std::vector<std::vector<int>> left_strong_intervals(const std::vector<int>& p) {
             const int n = p.size();
 
-            internal::permutation_tree::RangeAddRangeMin seg(n);
+            struct DoublyLinkedList {
+                using list_node_pointer = DoublyLinkedList*;
+                int idx, delta, max_idx, min_idx;
+                list_node_pointer prv = nullptr, nxt = nullptr;
+                DoublyLinkedList(int idx) : idx(idx) {}
+            };
+            using list_node_pointer = DoublyLinkedList::list_node_pointer;
+            list_node_pointer tail = nullptr;
 
-            std::vector<std::pair<int, int>> max_val, min_val;
+            using minmax_stack = std::vector<std::pair<int, list_node_pointer>>;
+            minmax_stack max_val, min_val;
 
-            for (int i = 0; i < n; ++i) {
-                auto update = [&](std::vector<std::pair<int, int>>& vals, auto comp) {
-                    seg.set(i, { i, i });
-                    int start = i;
-                    while (vals.size()) {
-                        const auto& [val, idx] = vals.back();
-                        if (comp(val, p[i])) break;
-                        seg.apply(idx, start, std::abs(val - p[i]));
-                        start = idx;
-                        vals.pop_back();
-                    }
-                    vals.emplace_back(p[i], start);
-                };
-                update(max_val, std::greater<int>());
-                update(min_val, std::less<int>());
+            auto erase = [&](list_node_pointer node) -> list_node_pointer {
+                list_node_pointer nxt = node->nxt, prv = node->prv;
+                if (list_node_pointer& nl = max_val[node->max_idx].second; node == nl) nl = nxt and nxt->max_idx == node->max_idx ? nxt : nullptr;
+                if (list_node_pointer& nl = min_val[node->min_idx].second; node == nl) nl = nxt and nxt->min_idx == node->min_idx ? nxt : nullptr;
+                if (nxt) nxt->prv = prv, nxt->delta += node->delta;
+                if (prv) prv->nxt = nxt;
+                delete node;
+                return nxt;
+            };
+            auto pop_tail = [&] { erase(std::exchange(tail, tail->prv)); };
 
-                PermutationTree node{ i, p[i], p[i] + 1, true };
-                while (true) {
-                    if (st.empty()) break;
-                    if (auto& top_node = st.back(); top_node.try_add_child(node)) {
-                        node = std::move(top_node);
-                        st.pop_back();
-                    } else if (node.vl == top_node.vr or node.vr == top_node.vl) {
-                        node = PermutationTree {
-                            top_node.idx,
-                            std::min(node.vl, top_node.vl),
-                            std::max(node.vr, top_node.vr),
-                            true,
-                            std::vector<PermutationTree> { std::move(top_node), std::move(node) }
-                        };
-                        st.pop_back();
-                    } else {
-                        int st_sum = i + 1 - (node.vr - node.vl);
-                        const auto& [val, idx] = seg.prod(0, st_sum);
-                        if (val != i) break;
-                        int vl = node.vl, vr = node.vr;
-                        auto it = st.end();
-                        for (int erase_num = st_sum - idx; erase_num;) {
-                            --it;
-                            erase_num -= it->vr - it->vl;
-                            vl = std::min(vl, it->vl), vr = std::max(vr, it->vr);
+            std::vector<std::vector<int>> select(n);
+            for (int i = 0, pl = n; i < n; ++i) {
+                while (tail and tail->idx > pl) pop_tail();
+                list_node_pointer new_node = new DoublyLinkedList(i);
+                auto rec_update = [&](auto rec_update, minmax_stack& vals, list_node_pointer nr, auto comp) -> bool {
+                    if (vals.empty()) return false;
+                    auto [val, nl] = vals.back();
+                    if (comp(val, p[i])) return false;
+                    vals.pop_back();
+                    if (not nl) nl = nr;
+                    if (not rec_update(rec_update, vals, nl, comp)) {
+                        list_node_pointer new_tail = nullptr;
+                        if (nl) {
+                            new_tail = nl->prv;
+                            nl->delta -= std::abs(val - p[i]);
+                            while (nl != nr and nl->delta < 0) nl = erase(nl);
+                            if (nl == nr) {
+                                nl = new_node;
+                            } else {
+                                new_tail = nr ? nr->prv : tail;
+                            }
+                        } else {
+                            nl = new_node;
+                            new_tail = nr ? nr->prv : tail;
                         }
-                        std::vector<PermutationTree> ch;
-                        std::move(it, st.end(), std::back_inserter(ch));
-                        ch.push_back(std::move(node));
-                        st.erase(it, st.end());
-                        node = PermutationTree { ch.front().idx, vl, vr, false, std::move(ch) };
+                        for (list_node_pointer cur = nr; cur;) cur = erase(cur);
+                        vals.emplace_back(p[i], nl);
+                        tail = new_tail;
                     }
+                    return true;
+                };
+                if (not rec_update(rec_update, max_val, nullptr, std::greater<int>())) max_val.emplace_back(p[i], new_node);
+                if (not rec_update(rec_update, min_val, nullptr, std::less<int>()))    min_val.emplace_back(p[i], new_node);
+                new_node->max_idx = max_val.size() - 1;
+                new_node->min_idx = min_val.size() - 1;
+
+                auto splitter_num = [&](list_node_pointer node) {
+                    return (max_val[node->max_idx].first - min_val[node->min_idx].first) - (i - node->idx);
+                };
+
+                if (tail) {
+                    new_node->prv = tail;
+                    tail->nxt = new_node;
+                    new_node->delta = splitter_num(tail);
+                } else {
+                    new_node->delta = std::numeric_limits<int>::max() / 2;
                 }
-                st.push_back(std::move(node));
+                tail = new_node;
+                if (list_node_pointer& nl = min_val.back().second; not nl) nl = tail;
+                if (list_node_pointer& nl = max_val.back().second; not nl) nl = tail;
+
+                for (list_node_pointer cur = tail; cur and splitter_num(cur) == 0; cur = cur->prv) {
+                    select[i].push_back(cur->idx);
+                }
+                pl = select[i].back();
             }
-            return st.back();
+            while (tail) pop_tail();
+
+            return select;
+        }
+
+        static std::vector<std::vector<int>> right_strong_intervals(std::vector<int> p) {
+            const int n = p.size();
+            std::reverse(p.begin(), p.end());
+            std::vector<std::vector<int>> res(n);
+            auto tmp = left_strong_intervals(p);
+            for (int r = 0; r < n; ++r) for (int l : tmp[r]) res[n - l - 1].push_back(n - r - 1);
+            return res;
+        }
+
+        static bool connectable(const std::pair<int, int>& p, const std::pair<int, int>& q) {
+            return std::max(p.second, q.second) - std::min(p.first, q.first) == (p.second - p.first) + (q.second - q.first);
+        }
+
+        static PermutationTree build(const std::vector<int>& p) {
+            const int n = p.size();
+            std::vector<std::vector<int>> sel_l = left_strong_intervals(p);
+            std::vector<std::vector<int>> sel_r = right_strong_intervals(p);
+
+            std::vector<PermutationTree> ch;
+            ch.reserve(n);
+
+            // strong intervals are enumerated in post order of dfs.
+            for (int r = 0; r < n; ++r) {
+                // iterate on the intersection of sel_l[r] and sel_r[r]
+                for (auto it1 = sel_l[r].cbegin(), end1 = sel_l[r].cend(), it2 = sel_r[r].cbegin(), end2 = sel_r[r].cend(); ; ++it1, ++it2) {
+                    while (it1 != end1 and it2 != end2 and *it1 != *it2) ++(*it1 > *it2 ? it1 : it2);
+                    if (it1 == end1 or it2 == end2) break;
+                    const int l = *it1;
+                    if (l == r) { // leaf
+                        ch.push_back(PermutationTree(1, l, p[l]));
+                        continue;
+                    }
+                    int vl = std::numeric_limits<int>::max();
+                    auto it = ch.end();
+                    while ((--it)->il != l) vl = std::min(vl, it->vl);
+                    PermutationTree t(r - l + 1, l, std::min(vl, it->vl));
+                    t.resize(ch.end() - it);
+                    std::move(it, ch.end(), t.begin());
+                    t._is_join_node = connectable(t[0].get_value_range(), t[1].get_value_range());
+                    ch.erase(it, ch.end());
+                    ch.push_back(std::move(t));
+                }
+            }
+            return std::move(ch.back());
         }
     };
-
 } // namespace suisen
-
 
 #endif // SUISEN_PERMUTATION_TREE
