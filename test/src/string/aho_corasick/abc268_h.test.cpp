d3f18fa8821d40313ff6e12fc20fd6594a3fa33d
suisen-cp <suisen.13107f4@gmail.com>
2022-10-15 19:53:20 +0900

    add test for Aho Corasick

---
+++ test/src/string/aho_corasick/abc268_h.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://atcoder.jp/contests/abc268/tasks/abc268_Ex"
+
+#include <iostream>
+
+#include "library/string/aho_corasick.hpp"
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+ 
+    std::string s;
+    std::cin >> s;
+    int n;
+    std::cin >> n;
+ 
+    suisen::AhoCorasick ac;
+    for (int i = 0; i < n; ++i) {
+        std::string t;
+        std::cin >> t;
+        ac.add(t);
+    }
+    ac.build();
+ 
+    int ans = 0;
+    int state = ac.init_state();
+    for (char c : s) {
+        if (int next_state = ac.next_state(state, c); ac.count_suffix_matching(next_state)) {
+            ++ans;
+            state = ac.init_state();
+        } else {
+            state = next_state;
+        }
+    }
+    std::cout << ans << std::endl;
+ 
+    return 0;
+}
