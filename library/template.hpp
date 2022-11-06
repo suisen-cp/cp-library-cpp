9ac1dceedae8d3a96573acf9c09679af5b875c26
suisen-cp <suisen.13107f4@gmail.com>
2022-11-06 23:07:33 +0900

    update macro

--- library/template.hpp
+++ library/template.hpp
@@ -14,35 +14,37 @@ using u128 = __uint128_t;
 
 template <typename T>
 using pq_greater = std::priority_queue<T, std::vector<T>, std::greater<T>>;
-template <typename T, typename U>
-using umap = std::unordered_map<T, U>;
 
-// ! macros (capital: internal macro)
-#define OVERLOAD2(_1,_2,name,...) name
-#define OVERLOAD3(_1,_2,_3,name,...) name
-#define OVERLOAD4(_1,_2,_3,_4,name,...) name
+// ! macros (internal)
+#define DETAIL_OVERLOAD2(_1,_2,name,...) name
+#define DETAIL_OVERLOAD3(_1,_2,_3,name,...) name
+#define DETAIL_OVERLOAD4(_1,_2,_3,_4,name,...) name
 
-#define REP4(i,l,r,s)  for(std::remove_reference_t<std::remove_const_t<decltype(r)>>i=(l);i<(r);i+=(s))
-#define REP3(i,l,r)    REP4(i,l,r,1)
-#define REP2(i,n)      REP3(i,0,n)
-#define REPINF3(i,l,s) for(std::remove_reference_t<std::remove_const_t<decltype(l)>>i=(l);;i+=(s))
-#define REPINF2(i,l)   REPINF3(i,l,1)
-#define REPINF1(i)     REPINF2(i,0)
-#define RREP4(i,l,r,s) for(std::remove_reference_t<std::remove_const_t<decltype(r)>>i=(l)+fld((r)-(l)-1,s)*(s);i>=(l);i-=(s))
-#define RREP3(i,l,r)   RREP4(i,l,r,1)
-#define RREP2(i,n)     RREP3(i,0,n)
+#define DETAIL_REP4(i,l,r,s)  for(std::remove_reference_t<std::remove_const_t<decltype(r)>>i=(l);i<(r);i+=(s))
+#define DETAIL_REP3(i,l,r)    DETAIL_REP4(i,l,r,1)
+#define DETAIL_REP2(i,n)      DETAIL_REP3(i,0,n)
+#define DETAIL_REPINF3(i,l,s) for(std::remove_reference_t<std::remove_const_t<decltype(l)>>i=(l);;i+=(s))
+#define DETAIL_REPINF2(i,l)   DETAIL_REPINF3(i,l,1)
+#define DETAIL_REPINF1(i)     DETAIL_REPINF2(i,0)
+#define DETAIL_RREP4(i,l,r,s) for(std::remove_reference_t<std::remove_const_t<decltype(r)>>i=(l)+fld((r)-(l)-1,s)*(s);i>=(l);i-=(s))
+#define DETAIL_RREP3(i,l,r)   DETAIL_RREP4(i,l,r,1)
+#define DETAIL_RREP2(i,n)     DETAIL_RREP3(i,0,n)
 
-#define rep(...)    OVERLOAD4(__VA_ARGS__, REP4   , REP3   , REP2   )(__VA_ARGS__)
-#define rrep(...)   OVERLOAD4(__VA_ARGS__, RREP4  , RREP3  , RREP2  )(__VA_ARGS__)
-#define repinf(...) OVERLOAD3(__VA_ARGS__, REPINF3, REPINF2, REPINF1)(__VA_ARGS__)
+#define DETAIL_CAT_I(a, b) a##b
+#define DETAIL_CAT(a, b) DETAIL_CAT_I(a, b)
+#define DETAIL_UNIQVAR(tag) DETAIL_CAT(tag, __LINE__)
 
-#define CAT_I(a, b) a##b
-#define CAT(a, b) CAT_I(a, b)
-#define UNIQVAR(tag) CAT(tag, __LINE__)
-#define loop(n) for (std::remove_reference_t<std::remove_const_t<decltype(n)>> UNIQVAR(loop_variable) = n; UNIQVAR(loop_variable) --> 0;)
+// ! macros
+#define REP(...)    DETAIL_OVERLOAD4(__VA_ARGS__, DETAIL_REP4   , DETAIL_REP3   , DETAIL_REP2   )(__VA_ARGS__)
+#define RREP(...)   DETAIL_OVERLOAD4(__VA_ARGS__, DETAIL_RREP4  , DETAIL_RREP3  , DETAIL_RREP2  )(__VA_ARGS__)
+#define REPINF(...) DETAIL_OVERLOAD3(__VA_ARGS__, DETAIL_REPINF3, DETAIL_REPINF2, DETAIL_REPINF1)(__VA_ARGS__)
 
-#define all(iterable) std::begin(iterable), std::end(iterable)
-#define input(type, ...) type __VA_ARGS__; read(__VA_ARGS__)
+#define LOOP(n) for (std::remove_reference_t<std::remove_const_t<decltype(n)>> DETAIL_UNIQVAR(loop_variable) = n; DETAIL_UNIQVAR(loop_variable) --> 0;)
+
+#define ALL(iterable) std::begin(iterable), std::end(iterable)
+#define INPUT(type, ...) type __VA_ARGS__; read(__VA_ARGS__)
+
+// ! debug
 
 #ifdef LOCAL
 #  define debug(...) debug_internal(#__VA_ARGS__, __VA_ARGS__)
@@ -113,13 +115,10 @@ std::ostream& operator<<(std::ostream& out, const std::pair<T, U>& a) {
 // tuple
 template <unsigned int N = 0, typename ...Args>
 std::ostream& operator<<(std::ostream& out, const std::tuple<Args...>& a) {
-    if constexpr (N >= std::tuple_size_v<std::tuple<Args...>>) {
-        return out;
-    } else {
+    if constexpr (N >= std::tuple_size_v<std::tuple<Args...>>) return out;
+    else {
         out << std::get<N>(a);
-        if constexpr (N + 1 < std::tuple_size_v<std::tuple<Args...>>) {
-            out << ' ';
-        }
+        if constexpr (N + 1 < std::tuple_size_v<std::tuple<Args...>>) out << ' ';
         return operator<<<N + 1>(out, a);
     }
 }
@@ -157,13 +156,13 @@ auto print_all(const Iterable& v, std::string sep = " ", std::string end = "\n")
     std::cout << end;
 }
 
-__int128_t parse_i128(std::string& s) {
+__int128_t stoi128(const std::string& s) {
     __int128_t ret = 0;
     for (int i = 0; i < int(s.size()); i++) if ('0' <= s[i] and s[i] <= '9') ret = 10 * ret + s[i] - '0';
     if (s[0] == '-') ret = -ret;
     return ret;
 }
-__uint128_t parse_u128(std::string& s) {
+__uint128_t stou128(const std::string& s) {
     __uint128_t ret = 0;
     for (int i = 0; i < int(s.size()); i++) if ('0' <= s[i] and s[i] <= '9') ret = 10 * ret + s[i] - '0';
     return ret;
@@ -172,14 +171,14 @@ __uint128_t parse_u128(std::string& s) {
 std::istream& operator>>(std::istream& in, __int128_t& v) {
     std::string s;
     in >> s;
-    v = parse_i128(s);
+    v = stoi128(s);
     return in;
 }
 // __uint128_t
 std::istream& operator>>(std::istream& in, __uint128_t& v) {
     std::string s;
     in >> s;
-    v = parse_u128(s);
+    v = stou128(s);
     return in;
 }
 // pair
@@ -190,11 +189,8 @@ std::istream& operator>>(std::istream& in, std::pair<T, U>& a) {
 // tuple
 template <unsigned int N = 0, typename ...Args>
 std::istream& operator>>(std::istream& in, std::tuple<Args...>& a) {
-    if constexpr (N >= std::tuple_size_v<std::tuple<Args...>>) {
-        return in;
-    } else {
-        return operator>><N + 1>(in >> std::get<N>(a), a);
-    }
+    if constexpr (N >= std::tuple_size_v<std::tuple<Args...>>) return in;
+    else return operator>><N + 1>(in >> std::get<N>(a), a);
 }
 // vector
 template <typename T>
@@ -216,82 +212,48 @@ void read(Args &...args) {
 // ! integral utilities
 
 // Returns pow(-1, n)
-template <typename T>
-constexpr inline int pow_m1(T n) {
+template <typename T> constexpr inline int pow_m1(T n) {
     return -(n & 1) | 1;
 }
 // Returns pow(-1, n)
-template <>
-constexpr inline int pow_m1<bool>(bool n) {
+template <> constexpr inline int pow_m1<bool>(bool n) {
     return -int(n) | 1;
 }
 
 // Returns floor(x / y)
-template <typename T>
-constexpr inline T fld(const T x, const T y) {
+template <typename T> constexpr inline T fld(const T x, const T y) {
     return (x ^ y) >= 0 ? x / y : (x - (y + pow_m1(y >= 0))) / y;
 }
-template <typename T>
-constexpr inline T cld(const T x, const T y) {
+template <typename T> constexpr inline T cld(const T x, const T y) {
     return (x ^ y) <= 0 ? x / y : (x + (y + pow_m1(y >= 0))) / y;
 }
 
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 16>> = nullptr>
+template <typename T, std::enable_if_t<std::negation_v<suisen::is_nbit<T, 64>>, std::nullptr_t> = nullptr>
 __attribute__((target("popcnt"))) constexpr inline int popcount(const T x) { return _mm_popcnt_u32(x); }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 32>> = nullptr>
-__attribute__((target("popcnt"))) constexpr inline int popcount(const T x) { return _mm_popcnt_u32(x); }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 64>> = nullptr>
+template <typename T, std::enable_if_t<suisen::is_nbit_v<T, 64>, std::nullptr_t> = nullptr>
 __attribute__((target("popcnt"))) constexpr inline int popcount(const T x) { return _mm_popcnt_u64(x); }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 16>> = nullptr>
+template <typename T, std::enable_if_t<std::negation_v<suisen::is_nbit<T, 64>>, std::nullptr_t> = nullptr>
 constexpr inline int count_lz(const T x) { return x ? __builtin_clz(x) : suisen::bit_num<T>; }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 32>> = nullptr>
-constexpr inline int count_lz(const T x) { return x ? __builtin_clz(x) : suisen::bit_num<T>; }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 64>> = nullptr>
+template <typename T, std::enable_if_t<suisen::is_nbit_v<T, 64>, std::nullptr_t> = nullptr>
 constexpr inline int count_lz(const T x) { return x ? __builtin_clzll(x) : suisen::bit_num<T>; }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 16>> = nullptr>
+template <typename T, std::enable_if_t<std::negation_v<suisen::is_nbit<T, 64>>, std::nullptr_t> = nullptr>
 constexpr inline int count_tz(const T x) { return x ? __builtin_ctz(x) : suisen::bit_num<T>; }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 32>> = nullptr>
-constexpr inline int count_tz(const T x) { return x ? __builtin_ctz(x) : suisen::bit_num<T>; }
-template <typename T, suisen::constraints_t<suisen::is_nbit<T, 64>> = nullptr>
+template <typename T, std::enable_if_t<suisen::is_nbit_v<T, 64>, std::nullptr_t> = nullptr>
 constexpr inline int count_tz(const T x) { return x ? __builtin_ctzll(x) : suisen::bit_num<T>; }
-template <typename T>
-constexpr inline int floor_log2(const T x) { return suisen::bit_num<T> -1 - count_lz(x); }
-template <typename T>
-constexpr inline int ceil_log2(const T x) { return floor_log2(x) + ((x & -x) != x); }
-template <typename T>
-constexpr inline int kth_bit(const T x, const unsigned int k) { return (x >> k) & 1; }
-template <typename T>
-constexpr inline int parity(const T x) { return popcount(x) & 1; }
+template <typename T> constexpr inline int floor_log2(const T x) { return suisen::bit_num<T> - 1 - count_lz(x); }
+template <typename T> constexpr inline int ceil_log2(const T x) { return floor_log2(x) + ((x & -x) != x); }
+template <typename T> constexpr inline int kth_bit(const T x, const unsigned int k) { return (x >> k) & 1; }
+template <typename T> constexpr inline int parity(const T x) { return popcount(x) & 1; }
 
 // ! container
 
-template <typename T, typename Comparator, suisen::constraints_t<suisen::is_comparator<Comparator, T>> = nullptr>
+template <typename T, typename Comparator>
 auto priqueue_comp(const Comparator comparator) {
     return std::priority_queue<T, std::vector<T>, Comparator>(comparator);
 }
 
-template <typename Iterable>
-auto isize(const Iterable& iterable) -> decltype(int(iterable.size())) {
-    return iterable.size();
-}
-
-template <typename T, typename Gen, suisen::constraints_t<suisen::is_same_as_invoke_result<T, Gen, int>> = nullptr>
-auto generate_vector(int n, Gen generator) {
-    std::vector<T> v(n);
-    for (int i = 0; i < n; ++i) v[i] = generator(i);
-    return v;
-}
-template <typename T>
-auto generate_range_vector(T l, T r) {
-    return generate_vector(r - l, [l](int i) { return l + i; });
-}
-template <typename T>
-auto generate_range_vector(T n) {
-    return generate_range_vector(0, n);
-}
-
-template <typename T>
-void sort_unique_erase(std::vector<T>& a) {
+template <typename Container>
+void sort_unique_erase(Container& a) {
     std::sort(a.begin(), a.end());
     a.erase(std::unique(a.begin(), a.end()), a.end());
 }
@@ -301,7 +263,7 @@ auto foreach_adjacent_values(InputIterator first, InputIterator last, BiConsumer
     if (first != last) for (auto itr = first, itl = itr++; itr != last; itl = itr++) f(*itl, *itr);
 }
 template <typename Container, typename BiConsumer>
-auto foreach_adjacent_values(Container c, BiConsumer f) -> decltype(c.begin(), c.end(), void()) {
+auto foreach_adjacent_values(Container &&c, BiConsumer f) -> decltype(c.begin(), c.end(), void()) {
     foreach_adjacent_values(c.begin(), c.end(), f);
 }
 
@@ -311,22 +273,18 @@ auto foreach_adjacent_values(Container c, BiConsumer f) -> decltype(c.begin(), c
 // x <- min(x, y). returns true iff `x` has chenged.
 template <typename T>
 inline bool chmin(T& x, const T& y) {
-    if (y >= x) return false;
-    x = y;
-    return true;
+    return y >= x ? false : (x = y, true);
 }
 // x <- max(x, y). returns true iff `x` has chenged.
 template <typename T>
 inline bool chmax(T& x, const T& y) {
-    if (y <= x) return false;
-    x = y;
-    return true;
+    return y <= x ? false : (x = y, true);
 }
 
 template <typename T, std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
 std::string bin(T val, int bit_num = -1) {
     std::string res;
-    if (bit_num >= 0) {
+    if (bit_num != -1) {
         for (int bit = bit_num; bit-- > 0;) res += '0' + ((val >> bit) & 1);
     } else {
         for (; val; val >>= 1) res += '0' + (val & 1);
