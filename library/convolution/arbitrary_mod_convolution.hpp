9cc79160b88885dc7f666a6065987f9ae2737e2d
suisen-cp <suisen.13107f4@gmail.com>
2023-05-11 13:18:22 +0900

    improve arbitrary mod convolution

--- library/convolution/arbitrary_mod_convolution.hpp
+++ library/convolution/arbitrary_mod_convolution.hpp
@@ -50,6 +50,39 @@ namespace suisen {
         }
         return c;
     }
+
+    std::vector<__uint128_t> convolution_int(const std::vector<int> &a, const std::vector<int> &b) {
+        int n = int(a.size()), m = int(b.size());
+
+        auto check_nonnegative = [](int e) { return e >= 0; };
+        assert(std::all_of(a.begin(), a.end(), check_nonnegative));
+        assert(std::all_of(b.begin(), b.end(), check_nonnegative));
+
+        if (n == 0 or m == 0) return {};
+        if (std::min(n, m) <= 120) return internal::convolution_naive<int, __uint128_t>(a, b);
+
+        static constexpr long long MOD1 = 754974721;  // 2^24
+        static constexpr long long MOD2 = 167772161;  // 2^25
+        static constexpr long long MOD3 = 469762049;  // 2^26
+        static constexpr long long M1M2 = MOD1 * MOD2;
+        static constexpr long long INV_M1_MOD2 = atcoder::internal::inv_gcd(MOD1, MOD2).second;
+        static constexpr long long INV_M1M2_MOD3 = atcoder::internal::inv_gcd(M1M2, MOD3).second;
+
+        auto c1 = atcoder::convolution<MOD1>(a, b);
+        auto c2 = atcoder::convolution<MOD2>(a, b);
+        auto c3 = atcoder::convolution<MOD3>(a, b);
+        std::vector<__uint128_t> c(n + m - 1);
+        for (int i = 0; i < n + m - 1; ++i) {
+            // Garner's Algorithm
+            // X = x1 + x2 * m1 + x3 * m1 * m2
+            // x1 = c1[i], x2 = (c2[i] - x1) / m1 (mod m2), x3 = (c3[i] - x1 - x2 * m1) / m2 (mod m3)
+            int x1 = c1[i];
+            int x2 = (atcoder::static_modint<MOD2>(c2[i] - x1) * INV_M1_MOD2).val();
+            int x3 = (atcoder::static_modint<MOD3>(c3[i] - x1 - x2 * MOD1) * INV_M1M2_MOD3).val();
+            c[i] = x1 + x2 * MOD1 + __uint128_t(x3) * M1M2;
+        }
+        return c;
+    }
 } // namespace suisen
 
 
