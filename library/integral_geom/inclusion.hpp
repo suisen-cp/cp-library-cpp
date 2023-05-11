288f5dc310e7f21856e38775291768b7ac830e1d
suisen-cp <suisen.13107f4@gmail.com>
2023-05-11 13:21:20 +0900

    Inclusion

---
+++ library/integral_geom/inclusion.hpp
@@ -0,0 +1,8 @@
+#ifndef SUISEN_INT_GEOM_INCLUSION
+#define SUISEN_INT_GEOM_INCLUSION
+
+namespace suisen::integral_geometry {
+    enum class Inclusion { OUT, ON, IN };
+}
+
+#endif // SUISEN_INT_GEOM_INCLUSION
