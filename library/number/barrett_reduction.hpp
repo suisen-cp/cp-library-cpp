e2b28556b28fa6a300fbf80806e097af2c4793af
suisen-cp <suisen.13107f4@gmail.com>
2023-05-11 13:33:28 +0900

    Barrett Reduction

--- library/number/barrett_reduction.hpp
+++ library/number/barrett_reduction.hpp
@@ -1,6 +1,7 @@
 #ifndef SUISEN_BARRETT_REDUCTION
 #define SUISEN_BARRETT_REDUCTION
 
+#include <array>
 #include <cstdint>
 #include <utility>
 
@@ -9,17 +10,20 @@ namespace suisen {
         uint32_t m;
         uint64_t im;
         BarrettReduction() = default;
-        BarrettReduction(uint32_t m) : m(m), im(uint64_t(-1) / m + 1) {}
+        BarrettReduction(uint32_t m) : m(m), im(uint64_t(0x7fff'ffff'ffff'ffff) / m + 1) {}
 
+        // 0 <= n < 2**63
         std::pair<uint64_t, uint32_t> quorem(uint64_t n) const {
-            uint64_t q = uint64_t((__uint128_t(n) * im) >> 64);
+            uint64_t q = uint64_t((__uint128_t(n) * im) >> 63);
             int64_t r = n - q * m;
             if (r < 0) --q, r += m;
             return std::make_pair(q, r);
         }
+        // 0 <= n < 2**63
         uint32_t quo(uint64_t n) const {
             return quorem(n).first;
         }
+        // 0 <= n < 2**63
         uint32_t rem(uint64_t n) const {
             return quorem(n).second;
         }
