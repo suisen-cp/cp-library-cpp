5cc323f771186bc60fc654ce628bcfba262fb63f
suisen-cp <suisen.13107f4@gmail.com>
2023-09-06 20:35:51 +0900

    test

---
+++ test/src/math/bigint/addition_of_big_integers.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/addition_of_big_integers"
+
+#include <iostream>
+
+#include "library/math/bigint.hpp"
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t --> 0) {
+        using suisen::bigint;
+
+        bigint a, b;
+        std::cin >> a >> b;
+        std::cout << a + b << '\n';
+    }
+}
