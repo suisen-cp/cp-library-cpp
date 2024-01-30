b1da93e057d6d1dfdf191da519d9f92a8e43fb4a
suisen-cp <suisen.13107f4@gmail.com>
2024-01-30 20:59:02 +0900

    Shift of Sampling Points

--- library/polynomial/shift_of_sampling_points.hpp
+++ library/polynomial/shift_of_sampling_points.hpp
@@ -7,8 +7,9 @@
 #include "library/math/factorial.hpp"
 
 namespace suisen {
-    template <typename mint>
-    std::vector<mint> shift_of_sampling_points(const std::vector<mint>& ys, mint t, int m) {
+    template <typename mint, typename Convolve,
+        std::enable_if_t<std::is_invocable_r_v<std::vector<mint>, Convolve, std::vector<mint>, std::vector<mint>>, std::nullptr_t> = nullptr>
+    std::vector<mint> shift_of_sampling_points(const std::vector<mint>& ys, mint t, int m, const Convolve &convolve) {
         const int n = ys.size();
         factorial<mint> fac(std::max(n, m));
 
@@ -18,7 +19,7 @@ namespace suisen {
                 f[i] = ys[i] * fac.fac_inv(i);
                 g[i] = (i & 1 ? -1 : 1) * fac.fac_inv(i);
             }
-            std::vector<mint> b = atcoder::convolution(f, g);
+            std::vector<mint> b = convolve(f, g);
             b.resize(n);
             return b;
         }();
@@ -31,7 +32,7 @@ namespace suisen {
                 c[i] = prd * fac.fac_inv(i);
                 prd *= t - i;
             }
-            std::vector<mint> e = atcoder::convolution(b, c);
+            std::vector<mint> e = convolve(b, c);
             e.resize(n);
             return e;
         }();
@@ -42,10 +43,16 @@ namespace suisen {
 
         std::vector<mint> f(m);
         for (int i = 0; i < m; ++i) f[i] = fac.fac_inv(i);
-        std::vector<mint> res = atcoder::convolution(e, f);
+        std::vector<mint> res = convolve(e, f);
         res.resize(m);
         for (int i = 0; i < m; ++i) res[i] *= fac.fac(i);
         return res;
+    }
+
+    template <typename mint>
+    std::vector<mint> shift_of_sampling_points(const std::vector<mint>& ys, mint t, int m) {
+        auto convolve = [&](const std::vector<mint> &f, const std::vector<mint> &g) { return atcoder::convolution(f, g); };
+        return shift_of_sampling_points(ys, t, m, convolve);
     }
 } // namespace suisen
 
