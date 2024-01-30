cc1b33815e480f4a8b289fbbbe64caa14343a93f
suisen-cp <suisen.13107f4@gmail.com>
2024-01-30 20:59:32 +0900

    fix bug of Berlekamp Massey

--- library/polynomial/berlekamp_massey.hpp
+++ library/polynomial/berlekamp_massey.hpp
@@ -9,14 +9,15 @@ namespace suisen {
      * @brief Find linear recurrence in O(|s|^2) time
      * @tparam F Arbitrary field (operator +, -, *, /, +=, -=, *=, /= must be defined)
      * @param s Prefix of a linearly reccurent sequence
-     * @return The vector of length L+1 c s.t. c_0=1 and s_i=Sum[j=1,L]c_i*s_{i-j}=0 for all i>=L, where L is the minimum integer s.t. there exists such c of length L+1.
+     * @return The vector of length L+1 c s.t. c_0=1 and s_i=Sum[j=1,L]c_i*s_{i-j} for all i>=L, where L is the minimum integer s.t. there exists such c of length L+1.
      */
     template <typename F>
     std::vector<F> find_linear_recuurence(const std::vector<F>& s) {
         std::vector<F> B{ 1 }, C{ 1 };
         B.reserve(s.size()), C.reserve(s.size());
         F b = 1;
-        for (std::size_t N = 0, L = 0, x = 1; N < s.size(); ++N) {
+        std::size_t L = 0;
+        for (std::size_t N = 0, x = 1; N < s.size(); ++N) {
             F d = s[N];
             for (std::size_t i = 1; i <= L; ++i) d += C[i] * s[N - i];
             if (d == 0) {
@@ -34,8 +35,7 @@ namespace suisen {
                 }
             }
         }
-        while (C.size() and C.back() == 0) C.pop_back();
-        const std::size_t L = C.size() - 1;
+        C.resize(L + 1);
         for (std::size_t N = 1; N <= L; ++N) C[N] = -C[N];
         return C;
     }
